56c2278c28f0fdf13450f0a19eae9276b9667f76
inlee <inlee@realgroove.net>
2026-03-13 02:30:07 +0900

    refactor(Camera): :recycle: Camera의 up vector 자동계산 구현

--- src/Graphics/Scene/Camera.ts
+++ src/Graphics/Scene/Camera.ts
@@ -8,21 +8,21 @@ export default class Camera {
      *
      * @type {Vector3}
      */
-    eye: Vector3;
+    eye: Vector3 = new Vector3(0, 0, 5); // 기본값: (0, 0, 5)
 
     /**
      * 카메라가 바라보는 지점 벡터
      *
      * @type {Vector3}
      */
-    at: Vector3;
+    at: Vector3 = new Vector3(0, 0, 0); // 기본값: (0, 0, 0)
 
     /**
      * 카메라의 상향 벡터
      *
      * @type {Vector3}
      */
-    up: Vector3;
+    up: Vector3 = new Vector3(0, 1, 0); // 기본값: (0, 1, 0)
 
     /**
      * 카메라의 프러스텀 정보
@@ -37,13 +37,10 @@ export default class Camera {
      * @constructor
      * @param {Vector3} eye   카메라 위치 벡터
      * @param {Vector3} at    카메라가 바라보는 지점 벡터
-     * @param {Vector3} up    카메라의 상향 벡터
      * @param {Frustum} frustum 카메라의 프러스텀 정보
      */
-    constructor(eye: Vector3, at: Vector3, up: Vector3, frustum: Frustum) {
-        this.eye = eye;
-        this.at = at;
-        this.up = up;
+    constructor(eye: Vector3, at: Vector3, frustum: Frustum) {
+        this.setValues(eye, at);
         this.frustum = frustum;
     }
 
@@ -52,13 +49,25 @@ export default class Camera {
      *
      * @param {Vector3} eye 카메라 위치 벡터
      * @param {Vector3} at  카메라가 바라보는 지점 벡터
-     * @param {Vector3} up  카메라의 상향 벡터
      * @return {void}
      */
-    setValues(eye: Vector3, at: Vector3, up: Vector3): void {
+    setValues(eye: Vector3, at: Vector3): void {
         this.eye = eye;
         this.at = at;
-        this.up = up;
+        this.calculateUpVector(eye, at);
+    }
+
+    /**
+     * 카메라의 상향 벡터 계산
+     *
+     * @param {Vector3} eye 카메라 위치 벡터
+     * @param {Vector3} at  카메라가 바라보는 지점 벡터
+     * @return {void}
+     */
+    private calculateUpVector(eye: Vector3, at: Vector3): void {
+        const forward = at.subtract(eye).normalize();
+        const right = forward.cross(new Vector3(0, 1, 0)).normalize();
+        this.up = right.cross(forward).normalize();
     }
 
     /**
