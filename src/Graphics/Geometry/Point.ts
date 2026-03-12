5e3923b666e05469d6bba19a0e21356652d33569
inlee <inlee@realgroove.net>
2026-03-13 02:28:49 +0900

    refactor(Point): :recycle: 주석 수정 및 정점의 밝기 속성 추가

--- src/Graphics/Geometry/Point.ts
+++ src/Graphics/Geometry/Point.ts
@@ -3,41 +3,51 @@ import Color from "../Common/Color";
 
 export default class Point {
     /**
-     * 삼각형의 2D 위치 정보 (화면 좌표계)
+     * 정점의 2D 위치 정보 (화면 좌표계)
      *
      * @type {Vector2}
      */
     position: Vector2;
 
     /**
-     * 삼각형의 깊이 정보 (Z-Buffer에서 사용)
+     * 정점의 깊이 정보 (Z-Buffer에서 사용)
      *
      * @type {number}
      */
     depth: number;
 
     /**
-     * 삼각형의 색상 정보 (RGBA)
+     * 정점의 색상 정보 (RGBA)
      *
      * @type {Color}
      */
     color: Color;
 
     /**
+     * 정점의 밝기 정보 (0.0 ~ 1.0)
+     *
+     * @type {number}
+     */
+    intensity: number;
+
+    /**
      * Constructor
      *
      * @constructor
-     * @param {Vector2} position                        삼각형의 2D 위치 정보 (화면 좌표계)
-     * @param {number} [depth=0]                        삼각형의 깊이 정보 (Z-Buffer에서 사용, 기본값은 0)
-     * @param {Color} [color=new Color(0, 0, 0, 255)]   삼각형의 색상 정보 (RGBA, 기본값은 검정색)
+     * @param {Vector2} position                        정점의 2D 위치 정보 (화면 좌표계)
+     * @param {number} [depth=0]                        정점의 깊이 정보 (Z-Buffer에서 사용, 기본값은 0)
+     * @param {Color} [color=new Color(0, 0, 0, 255)]   정점의 색상 정보 (RGBA, 기본값은 검정색)
+     * @param {number} [intensity=1.0]                  정점의 밝기 정보 (0.0 ~ 1.0, 기본값은 1.0)
      */
     constructor(
         position: Vector2,
         depth: number = 0,
         color: Color = new Color(0, 0, 0, 255),
+        intensity: number = 1.0,
     ) {
         this.position = position;
         this.depth = depth;
         this.color = color;
+        this.intensity = Math.min(1.0, intensity);
     }
 }
