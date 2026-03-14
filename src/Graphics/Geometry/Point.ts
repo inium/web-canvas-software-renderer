c9da800982baf91ba6f2b5d451089e0c9f1574de
inlee <inlee@realgroove.net>
2026-03-14 20:06:38 +0900

    refactor(Point): :recycle: 정점 밝기 속성 삭제 - Renderer 내에서 사용하도록 수정

--- src/Graphics/Geometry/Point.ts
+++ src/Graphics/Geometry/Point.ts
@@ -24,30 +24,20 @@ export default class Point {
     color: Color;
 
     /**
-     * 정점의 밝기 정보 (0.0 ~ 1.0)
-     *
-     * @type {number}
-     */
-    intensity: number;
-
-    /**
      * Constructor
      *
      * @constructor
      * @param {Vector2} position                        정점의 2D 위치 정보 (화면 좌표계)
      * @param {number} [depth=0]                        정점의 깊이 정보 (Z-Buffer에서 사용, 기본값은 0)
      * @param {Color} [color=new Color(0, 0, 0, 255)]   정점의 색상 정보 (RGBA, 기본값은 검정색)
-     * @param {number} [intensity=1.0]                  정점의 밝기 정보 (0.0 ~ 1.0, 기본값은 1.0)
      */
     constructor(
         position: Vector2,
         depth: number = 0,
         color: Color = new Color(0, 0, 0, 255),
-        intensity: number = 1.0,
     ) {
         this.position = position;
         this.depth = depth;
         this.color = color;
-        this.intensity = Math.min(1.0, intensity);
     }
 }
