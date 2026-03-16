923611b3eb7009009e61523e2c26dbb5c9f62e3b
inlee <inlee@realgroove.net>
2026-03-16 18:22:12 +0900

    chore(commitlint): :wrench: commitlint - conventional commit 기본 속성만 사용하도록 수정

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,12 +1,3 @@
 module.exports = {
     extends: ["@commitlint/config-conventional"],
-    rules: {
-        "type-enum": [
-            2,
-            "always",
-            ["feat", "fix", "docs", "refactor", "test", "chore"],
-        ],
-        "scope-empty": [2, "never"],
-        "subject-empty": [2, "never"],
-    },
 };
