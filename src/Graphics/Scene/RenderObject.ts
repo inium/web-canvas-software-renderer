98cc698ccdbc2dcd048b435b255ae8685f5b7d69
inlee <inlee@realgroove.net>
2026-03-16 19:35:16 +0900

    refactor(RenderObject): :recycle: 미사용 Texture 주석 삭제

--- src/Graphics/Scene/RenderObject.ts
+++ src/Graphics/Scene/RenderObject.ts
@@ -25,25 +25,13 @@ export default class RenderObject {
     transformation: Transformation = new Transformation();
 
     /**
-     * 텍스처 (Texture)
-     *
-     * @type {Texture | null}
-     */
-    // texture: Texture | null = null;
-
-    /**
      * Constructor
      *
      * @constructor
      * @param {Mesh} mesh 메시 (Mesh)
      * @param {Material} [material=null] 재질 (Material, 기본값은 null)
-     * @param {Texture} [texture=null] 텍스처 (Texture, 기본값은 null)
      */
-    constructor(
-        mesh: Mesh,
-        material: Material | null = null,
-        // public texture: Texture | null = null,
-    ) {
+    constructor(mesh: Mesh, material: Material | null = null) {
         this.mesh = mesh;
         if (material) {
             this.material = material;
